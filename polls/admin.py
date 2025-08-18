20f9758b3b3fc91aad84ebca000aecd0dbe338f9
Valerio Franchi <vlrfranchi1@gmail.com>
2025-08-18 13:40:45 +0200

    admin fix part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Question
-admin.site.register(Question)
+from .models import Choice
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
